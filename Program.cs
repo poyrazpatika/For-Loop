0ee005062a1147d52f8fcea48703ef6bdd4b892c
yppyrz <yppyrz@gmail.com>
2022-01-16 16:25:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = int.Parse(Console.ReadLine());
+
+            // Ekrana girilen sayıya kadar olan tek sayıları ekrana yazdır.
+            for (int i = 0; i <= sayac; i++)
+            {
+                if (i%2 == 1)// tek sayı ise
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            int tektoplam = 0;
+            int cifttoplam = 0;
+            // 1 ile 1000 arasındaki tek ve çift sayıların kendi içlerindeki toplamlarını ekrana yazdır.
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i%2 == 1)
+                {
+                    // Tek sayılar
+                    tektoplam += i;
+                }
+                else
+                {
+                    // Çift sayılar
+                    cifttoplam += i;
+                }
+            }
+            Console.WriteLine("Tek toplam :" + tektoplam);
+            Console.WriteLine("Çift toplam :" + cifttoplam);
+
+
+            // break, continue
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break; // i = 4 olduğunda, döngüden tamamen çıkar.
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue; // i = 4 olduğu adımı atlar bir sonraki adıma geçer.
+                }
+            }
+
+            //for (; ;)
+            //{
+            //    // sonsuz döngü.
+            //}
+        }
+    }
+}
